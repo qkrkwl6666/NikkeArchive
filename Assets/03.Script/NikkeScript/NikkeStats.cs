41d4595adf982e0d1a38409a1da48535160a0e7c
박재홍 <qkrkwl6666@gmail.com>
2024-11-08 16:59:44 +0900

    라피 캐릭터 모델링 추가 및 애니메이션 적용 니케 클래스 추가 (개발 중) 니케 스텟 클래스 추가 (개발 중)

---
+++ Assets/03.Script/NikkeScript/NikkeStats.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NikkeStats
+{
+    public int Hp {  get; private set; } // 체력
+    public int Defence { get; private set; } // 방어력
+    public int Attack { get; private set; } // 공격력
+    public BustType BustType { get; set; } // 버스트 
+    public CodeChip Category { get; set; } // 속성
+    public Weapon Weapon { get; set; } // 무기
+
+}
+
+public enum BustType
+{
+    BUST_1 = 0,
+    BUST_2 = 1,
+    BUST_3 = 2,
+
+    COUNT,
+}
+public enum CodeChip
+{
+    FIRE = 0,     // 작열
+    WIND = 1,     // 풍압
+    IRON = 2,     // 철갑
+    ELECTRIC = 3, // 전격
+    WATER = 4,    // 수냉
+
+    COUNT,    
+}
+
+public enum Weapon
+{
+    AR = 0,
+    SMG = 1,
+    SG = 2,
+    SR = 3,
+    RL = 4,
+    MG = 5,
+
+    COUNT,
+}
